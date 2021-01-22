9ded76add83b317285f4ee860083deccda767c04
Tomislav Marinkovic <tmarinkovic.tm@gmail.com>
2021-01-22 17:16:25 +0100

    fix responsive issues

--- src/component/pair/pair.js
+++ src/component/pair/pair.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Pair = ({pair}) => {
     return (
-        <Grid item xs={3}>
+        <Grid item xs={6} sm={6} md={4} lg={3} xl={3}>
             <Paper key={`paper-${pair}`} className="paper">{pair[0]} - {pair[1]}</Paper>
         </Grid>
     )
--- src/component/session/session.js
+++ src/component/session/session.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export const Session = ({session, sessionNumber}) => {
     return (
-        <Grid className='session' container>
+        <Grid className='session' container xs={12} sm={8} md={8} lg={8} xl={8}>
             <span className="session-title">{sessionNumber}</span>
             {session.map(pair =>
                 <Pair
