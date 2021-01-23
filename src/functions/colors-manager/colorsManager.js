73c35510b210e2facf5a95468482666da6142ea1
Tomislav Marinkovic <tmarinkovic.tm@gmail.com>
2021-01-23 17:46:09 +0100

    update order of colors to be assigned

--- src/functions/colors-manager/colorsManager.js
+++ src/functions/colors-manager/colorsManager.js
@@ -3,17 +3,17 @@ export const getParticipantColors = pairs => {
 
     const colorList = [
         '#b9f261',
+        '#f76865',
+        '#41a6f6',
+        '#fae058',
+        '#73eff7',
+        '#94b0c2',
         '#99b851',
+        '#bf585e',
         '#61f2ca',
         '#51b892',
-        '#fae058',
         '#ffbf54',
-        '#f76865',
-        '#bf585e',
-        '#41a6f6',
-        '#73eff7',
         '#f4f4f4',
-        '#94b0c2',
         '#ffcd75',
         '#38b764',
     ]
