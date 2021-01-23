4f2aa05d48c08cbdfa6f5666e47ea8a8cbd3a8a6
Tomislav Marinkovic <tmarinkovic.tm@gmail.com>
2021-01-23 17:43:44 +0100

    fix tests

--- src/functions/macher/matcher.test.js
+++ src/functions/macher/matcher.test.js
@@ -1,32 +1,33 @@
-const matcher = require('./Matcher').default;
+import matcher from "./Matcher";
 
 const evenNumberOfParticipants = ["A","B","C","D","E","F"]
 const oddNumberOfParticipants = ["A","B","C","D","E","F","G"]
 
 
-test('create pairs for even number of participants', () => {
-    const result = matcher(evenNumberOfParticipants)
-    let allPairs = []
-    expect(result.length).toBe(5)
-    result.forEach(session => {
-        expect(session.length).toBe(3)
-        session.forEach(pair => {
-            allPairs.push(pair)
-            expect(pair.length).toBe(2)
+test('create pairs for even number of participants',  done => {
+    matcher(evenNumberOfParticipants)
+        .then(result => {
+            expect(result.length).toBe(5)
+            result.forEach(session => {
+                expect(session.length).toBe(3)
+                session.forEach(pair => {
+                    expect(pair.length).toBe(2)
+                })
+            })
+            done()
         })
-    })
 });
 
-test('create pairs for odd number of participants', () => {
-    const result = matcher(oddNumberOfParticipants)
-    let allPairs = []
-    expect(result.length).toBe(7)
-    result.forEach(session => {
-        expect(session.length).toBe(4)
-        session.forEach(pair => {
-            allPairs.push(pair)
-            expect(pair.length).toBe(2)
+test('create pairs for odd number of participants', done => {
+    matcher(oddNumberOfParticipants)
+        .then(result => {
+            expect(result.length).toBe(7)
+            result.forEach(session => {
+                expect(session.length).toBe(4)
+                session.forEach(pair => {
+                    expect(pair.length).toBe(2)
+                })
+            })
+            done()
         })
-    })
-
 });
